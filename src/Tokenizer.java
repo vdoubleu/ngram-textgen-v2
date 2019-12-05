19c16436ebbd6f5828bf8d538b3b5a1a65f6d3f0
Victor Wang <victorwang2001@gmail.com>
2019-12-05 13:55:12 -0500

    completed filling dictionary and intaking start prompt

---
+++ src/Tokenizer.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class Tokenizer {
+	int n;
+	String filePath;
+	File dataFile;
+	Scanner scan;
+	ArrayList<String> words = new ArrayList<String>();
+	
+	public Tokenizer(String path, int depth){
+		this.n = depth + 1;
+		this.filePath = path;
+		this.dataFile = new File(this.filePath);
+		try {
+			scan = new Scanner(dataFile);
+		
+			for(int i = 0; i < n; i++)
+				this.words.add(scan.next());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean hasNextToken(){
+		return scan.hasNext();
+	}
+	
+	public void getNextToken(){
+			this.words.remove(0);
+			this.words.add(scan.next());
+	}
+	
+	public ArrayList<String> getWords(){
+		return words;
+	}
+	
+	public void resetUpdateTokenizer(int depth){
+		this.n = depth + 1;
+		
+		try {
+			scan = new Scanner(dataFile);
+		
+			for(int i = 0; i < n; i++)
+				this.words.add(scan.next());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+}
--- src/textPredictor.java
+++ src/textPredictor.java
@@ -6,34 +6,46 @@ public class textPredictor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		String path = "./src/inputData.txt";
+
 		Dictionary dic = new Dictionary();
+		Scanner scan = new Scanner(System.in);
 		
-		WordTrie t = new WordTrie();
+		int depth;
 		
-		ArrayList<String> lst = new ArrayList<String>();
-		lst.add("1st");
-		lst.add("2nd");
-		lst.add("3rd");
-		lst.add("4th");
-		/*
-		WordTrie.addSeq(t, lst);
-		*/
-		/*ArrayList<String> lst = new ArrayList<String>();
-		lst.add("1st");
-		lst.add("2nd");
-		lst.add("3rd");
+		System.out.println("What is your desired depth of search?");
+		depth = scan.nextInt();
+		scan.nextLine();
+		
+		Tokenizer token = new Tokenizer(path, 1);
 
 		
-		WordTrie.addSeq(t, lst);
+		//fill dictionary with words
+		for(int i = 2; i < depth + 1; i++){
+			while(token.hasNextToken()){
+				dic.updateDic(token.getWords());
+				token.getNextToken();
+			}
+			token.resetUpdateTokenizer(i);
+		}
 		
 		
-		System.out.println(WordTrie.seqFreq(t, lst));*/
-		dic.updateDic(lst);
-		dic.updateDic(lst);
-		dic.updateDic(lst);
+		// outputting
+		ArrayList<String> wordHist;
+		String inputStr;
 		
+		System.out.println("input starting prompt, the word count must be greater than or equal to " + depth);
+		inputStr = scan.nextLine();
 		
-		System.out.println(dic.getFreq(lst));
+		wordHist = new ArrayList<String>(Arrays.asList(inputStr.split(" ")));
+		
+		while(wordHist.size() > depth)
+			wordHist.remove(0);
+		
+		for(int i = 0; i < wordHist.size(); i++){
+			System.out.println(wordHist.get(i));
+		}
+		
 	}
 
 }
