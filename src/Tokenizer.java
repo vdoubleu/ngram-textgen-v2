7f5342ec0e7fab8ee6a1135e428ee3a3211c774c
Victor Wang <victorwang2001@gmail.com>
2019-12-06 01:53:44 -0500

    fixed merge conflicts

--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -9,28 +9,21 @@ public class Tokenizer {
 	ArrayList<String> words = new ArrayList<String>();
 	
 	public Tokenizer(String path, int depth){
-<<<<<<< HEAD
 		this.n = depth + 1;
-=======
->>>>>>> c1cf957baf4339661fd0b18455f959df8b485b4b
+
 		this.filePath = path;
 		this.dataFile = new File(this.filePath);
 		try {
 			scan = new Scanner(dataFile);
-<<<<<<< HEAD
-		
+	
 			for(int i = 0; i < n; i++)
 				this.words.add(scan.next());
 		} catch (FileNotFoundException e) {
-=======
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
->>>>>>> c1cf957baf4339661fd0b18455f959df8b485b4b
+
 			e.printStackTrace();
 		}
 	}
 	
-<<<<<<< HEAD
 	public boolean hasNextToken(){
 		return scan.hasNext();
 	}
@@ -57,8 +50,5 @@ public class Tokenizer {
 		}
 	}
 	
-	
-	
-=======
->>>>>>> c1cf957baf4339661fd0b18455f959df8b485b4b
+
 }
